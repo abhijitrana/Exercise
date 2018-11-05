479ab8c370af6c2f63e5f578b5a96ed48c2503d0
Umesh Verma <umeshiscreative@gmail.com>
2018-11-06 05:11:00 +0530

    Initial Commit

---
+++ Excercise/Excercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Excercise
+//
+//  Created by Umesh Verma on 06/11/18.
+//  Copyright © 2018 Umesh Verma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
