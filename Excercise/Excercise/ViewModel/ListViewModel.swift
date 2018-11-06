e31784fda78539afabc291814b45458d518f4e96
Umesh Verma <umeshiscreative@gmail.com>
2018-11-06 06:03:21 +0530

    Base View Model added for Our View Models

--- Excercise/Excercise/ViewModel/ListViewModel.swift
+++ Excercise/Excercise/ViewModel/ListViewModel.swift
@@ -8,6 +8,53 @@
 
 import Foundation
 
-class ListViewModel {
+class BaseViewModel {
     
+    // MARK: UI
+    var isLoading: Bool = false {
+        didSet {
+            showLoading?()
+        }
+    }
+    var showLoading: (() -> Void)?
+    var reloadData: (() -> Void)?
+    var showError: ((Error) -> Void)?
+    
+}
+
+class ListViewModel: BaseViewModel {
+    
+    // MARK: Properties
+    private let client: APIClient
+    
+    // MARK: - Data Array
+    var dataArray: [ItemData] = []
+    
+    // MARK: - Dependency Injections
+    init(client: APIClient) {
+        self.client = client
+    }
+    
+    // MARK: - Fetch API Data
+    func fetchData() {
+        
+        //Loader is visible
+        self.isLoading = true
+        
+        // Fetch API Data
+        self.client.fetch { either in
+            switch either {
+            case .success(let data):
+                self.dataArray = []
+                self.dataArray = data.rows
+            case .error(let error):
+                self.showError?(error)
+            }
+            //Hide Loader
+            self.isLoading = false
+            
+            //Reload Tableview
+            self.reloadData?()
+        }
+    }
 }
